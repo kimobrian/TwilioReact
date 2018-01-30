b813613490231108a807c88837407deeab7de385
kimobrian <brian.kimokoti@andela.com>
2018-01-31 00:20:40 +0300

    feat(server): Include code to generate token on server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var path = require("path");
 var express = require("express");
 var webpack = require("webpack");
 var faker = require("faker");
+var AccessToken = require("twilio").jwt.AccessToken;
+var VideoGrant = AccessToken.VideoGrant;
 
 var app = express();
 if(process.env.NODE_ENV === "DEV") { // Configuration for development environment
@@ -18,6 +20,38 @@ if(process.env.NODE_ENV === "DEV") { // Configuration for development environmen
 } else if(process.env.NODE_ENV === "PROD") { // Configuration for production environment
     app.use(express.static(path.join(__dirname, "dist")));
 }
+
+app.use(function(req, res, next){
+    console.log("Request from: ", req.url);
+    next();
+})
+
+// Endpoint to generate access token
+app.get("/token", function(request, response) {
+    var identity = faker.name.findName();
+
+    // Create an access token which we will sign and return to the client,
+    // containing the grant we just created
+    var token = new AccessToken(
+        process.env.TWILIO_ACCOUNT_SID,
+        process.env.TWILIO_API_KEY,
+        process.env.TWILIO_API_SECRET
+    );
+
+    // Assign the generated identity to the token
+    token.identity = identity;
+
+    const grant = new VideoGrant();
+   // Grant token access to the Video API features
+   token.addGrant(grant);
+
+   // Serialize the token to a JWT string and include it in a JSON response
+   response.send({
+       identity: identity,
+       token: token.toJwt()
+   });
+});
+
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
